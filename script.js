b1bf429d628b52a8bd59c362cbe2aaa3a45f93dc
Χένρικ-Js <henrrikjesus07@gmail.com>
2026-03-16 23:42:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const log = console.log
+let price = 3.50
+log("batata"+" "+"R$"+price)
+log("batata + arroz?")
+let arroz = 3.50*2
+log("arroz ="+" "+arroz)
+log("batata + arroz ="+" "+(price+arroz))
